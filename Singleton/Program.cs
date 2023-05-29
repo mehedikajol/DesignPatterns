ededf4a22abe683efdcdbe358121174d669fe38e
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-05-29 21:16:53 +0600

    singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var instance11 = SingletonUnsafe.GetInstance();
+        instance11.PrintMessage("Instance 01");
+        var instance12 = SingletonUnsafe.GetInstance();
+        instance12.PrintMessage("Instance 02");
+
+        var instance21 = SingletonSafe.GetInstance();
+        instance21.PrintMessage("Instance 01");
+        var instance22 = SingletonSafe.GetInstance();
+        instance22.PrintMessage("Instance 02");
+    }
+}
